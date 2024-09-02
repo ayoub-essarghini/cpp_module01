0f5bb6060d35dfc7356eddc2c0ef50fc5dba2301
roboto <ayoubmajd311@gmail.com>
2024-09-02 03:33:37 +0100

    fix ex06

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -1,45 +1,59 @@
 #include "Harl.hpp"
+#include <string>
 
-Harl::Harl()
-{
-}
+Harl::Harl() {}
 
-Harl::~Harl()
-{
-}
+Harl::~Harl() {}
 
 void Harl::debug(void)
 {
-    std::cout << _GREEN << "DEBUG :" << _RESET << std::endl << "     Hello, Iam debug message " << std::endl;
+    std::cout << _GREEN << "DEBUG :" << _RESET << std::endl
+              << "  Hello, I am debug message " << std::endl;
 }
 
 void Harl::info(void)
 {
-    std::cout << _CYAN << "INFO :"<< _RESET << std::endl << "    Hi, Iam info message " <<std::endl;
+    std::cout << _CYAN << "INFO :" << _RESET << std::endl
+              << "    Hi, I am info message " << std::endl;
 }
 
 void Harl::warning(void)
 {
-    std::cout << _YELLOW << "WARNING :" << _RESET<< std::endl << "   Hello Iam warning message "<<std::endl;
+    std::cout << _YELLOW << "WARNING :" << _RESET << std::endl
+              << "   Hello, I am warning message " << std::endl;
 }
 
 void Harl::error(void)
 {
-    std::cout << _RED << "ERROR :"<< _RESET << std::endl << "    Hello Iam error message "<<std::endl;;
-}
-
-void nofunc(std::string name)
-{
-    std::cout << " There is no function called : \" " << _YELLOW << name << _RESET << "\""<< std::endl;
+    std::cout << _RED << "ERROR :" << _RESET << std::endl
+              << "    Hello, I am error message " << std::endl;
 }
 
 void Harl::complain(std::string level)
 {
-    void (Harl::*funcPtr)() = NULL;
 
-    funcPtr = (level == "DEBUG") ? &Harl::debug : (level == "INFO")  ? &Harl::info
-                                              : (level == "WARNING") ? &Harl::warning
-                                              : (level == "ERROR")   ? &Harl::error
-                                                                     : NULL;
-    funcPtr != NULL ? (this->*funcPtr)() : nofunc(level);
+    std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    void (Harl::*functions[])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+
+    int i = 0;
+    while (i < 4 && levels[i] != level)
+        i++;
+
+    switch (i)
+    {
+    case 0:
+        (this->*functions[0])();
+        break;
+    case 1:
+        (this->*functions[1])();
+        break;
+    case 2:
+        (this->*functions[2])();
+        break;
+    case 3:
+        (this->*functions[3])();
+        break;
+    default:
+        std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+    }
 }
